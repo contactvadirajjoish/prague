572432db35aaf19dd762d48e552ef7becd0ebd01
Joish Ramamurthy <joish.vadiraj@thermofisher.com>
2020-10-10 15:44:12 +0200

    whatever

--- variables.py
+++ variables.py
@@ -1,1 +1,10 @@
-##Variables
+x=10  
+print(x)
+#it will print variable value which has been declared
+a = 20.3
+b= "Hello World"
+print(a,b)
+#Varicables can hold float and string value as well
+x = "Anjali"
+print(x)
+#Variable can be redefined 
